51285ddbe6ce6293bddfca2a30b73610e9c255c0
Mati Melkamu <matimelkamu15@gmail.com>
2025-08-27 22:47:17 +0300

    add a title for the about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,13 @@
 function About() {
-  return <section className="h-screen"></section>;
+  return (
+    <section className="h-screen overflow-hidden relative  bg-gradient-to-b  from-black to- bg-gray-900">
+      <div>
+        <h1 className="text-4xl md:text-6xl font-bold sm:mb-16  text-center  text-white">
+          About Me
+        </h1>
+      </div>
+    </section>
+  );
 }
 
 export default About;
