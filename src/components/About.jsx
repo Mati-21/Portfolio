77d8f293b75d27fe34a60aa5d72130be25944c49
Mati Melkamu <matimelkamu15@gmail.com>
2025-08-30 22:31:28 +0300

    add animation to the about me section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,18 @@
+import { motion } from "framer-motion";
+
 function About() {
   return (
-    <section className="h-screen overflow-hidden relative  bg-gradient-to-b  from-black to- bg-gray-900">
+    <section className="h-screen overflow-hidden relative p-10 bg-gradient-to-b  from-black to- bg-gray-900">
       <div>
-        <h1 className="text-4xl md:text-6xl font-bold sm:mb-16  text-center  text-white">
+        <motion.h1
+          initial={{ opacity: 0, y: 500 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+          viewport={{ amount: 0.5 }}
+          className="text-4xl md:text-6xl font-bold sm:mb-16  text-center  text-white"
+        >
           About Me
-        </h1>
+        </motion.h1>
       </div>
     </section>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -42,10 +42,11 @@ function Header() {
                 transition={{ duration: 0.3, delay: 0.7 + index * 0.2 }}
                 href={`#${item.toLowerCase()}`}
                 key={index}
-                className="relative text-gray-800 dark:text-gray-200 hover:text-violet-600 dark:hover:text-violet-400 font-medium transition-colors duration-300 group"
+                className="relative text-gray-800 dark:text-gray-200 hover:text-violet-600 dark:hover:text-violet-400 font-medium transition-colors duration-300 group pb-1"
               >
                 {item}
-                <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-violet-600 group-hover:w-full transition-all duration-300"></span>
+                {/* underline */}
+                <span className="absolute bottom-0 left-0 w-0 h-[3px] bg-violet-600 dark:bg-violet-400 group-hover:w-full transition-all duration-300"></span>
               </motion.a>
             )
           )}
@@ -207,12 +208,11 @@ function Header() {
                 </div>
 
                 {/* Form Button */}
-
                 <motion.button
                   type="submit"
                   whileHover={{ scale: 1.03 }}
                   whileTap={{ scale: 0.97 }}
-                  className="w-full py-2 px-4 bg-gradient-to-r from-violet-600 to-violet-400 rounded-lg shadow-md hover:shadow-lg hover:from-violet-600 hover:to-purple-600 transition-all duration-300 text-white"
+                  className="w-full py-2 px-4 bg-gradient-to-r from-violet-600 to-violet-400 rounded-lg shadow-md hover:shadow-lg hover:from-violet-600 hover:to-purple-600 transition-all duration-300 text-white mt-3"
                 >
                   Send Message
                 </motion.button>
